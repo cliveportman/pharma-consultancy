f173be61d038d1381c7c1ab723c19a22e7b88769
cliveportman <clive.theportman.co>
2020-11-09 12:20:15 +0000

    Starting Team page

--- src/Pages/Team.svelte
+++ src/Pages/Team.svelte
@@ -1,6 +1,8 @@
 <script>
   import { url, options, teamData as pageData } from '../stores/global-store'
   import {query } from '../stores/graphql-queries/team-query'
+  import LoadingIcon from '../Components/LoadingIcon.svelte'
+  import TestimonialsCarousel from '../Components/TestimonialsCarousel.svelte'
 
   fetch(url, options(query))
   .then( (resp) => resp.json() )
