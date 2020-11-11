4f8326874c81b7fb029cc8f7075db077b8e03fc9
cliveportman <clive@cliveportman.co.uk>
2020-11-11 14:36:33 +0000

    Trying out SendGrid

--- src/Pages/Contact.svelte
+++ src/Pages/Contact.svelte
@@ -1,4 +1,23 @@
 <script>
   import { sitewideData } from '../stores/global-store'
 
+  
+  const url = '/.netlify/functions/sendgrid';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+      message: "Testing, testing..."
+    })
+  };
+
+fetch(url, options)
+  .then(response => {
+    console.log(response.status);
+  });
+
+
 </script>
