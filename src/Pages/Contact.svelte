1ee2cc5f67cfea8df8cfc758dcab5d928d4587f7
cliveportman <clive@cliveportman.co.uk>
2020-11-11 17:23:18 +0000

    Update Contact.svelte

--- src/Pages/Contact.svelte
+++ src/Pages/Contact.svelte
@@ -2,22 +2,22 @@
   import { sitewideData } from '../stores/global-store'
 
   
-  const url = '/.netlify/functions/sendgrid';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json;charset=UTF-8'
-    },
-    body: JSON.stringify({
-      message: "Testing, testing..."
-    })
-  };
 
-fetch(url, options)
-  .then(response => {
-    console.log(response.status);
-  });
+
+
+  let options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" }
+  }
+
+
+  fetch("https://silly-leavitt-9429a2.netlify.app/.netlify/functions/hello", options)
+  .then(function(resp) {
+
+    console.log(resp)
+
+   })
+  .catch(console.error);
 
 
 </script>
