e00a5470ba2a4effa57dbc324204f4c0a4d68ed5
cliveportman <clive@cliveportman.co.uk>
2020-11-11 17:18:31 +0000

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,17 +1,9 @@
 
 
 
-const headers = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-};
-
-
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    headers,
     body: "Hello, World 1"
   });
 };
