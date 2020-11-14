1853ca4a63f62662390ded3ff2cc3e311fa19a14
cliveportman <clive@cliveportman.co.uk>
2020-11-14 12:31:26 +0000

    Create sendgridnovars.js

---
+++ functions/sendgridnovars.js
@@ -0,0 +1,39 @@
+const client = require('@sendgrid/mail')
+
+client.setApiKey(process.env.SENDGRID_API_KEY)
+
+function sendEmail(client, message, senderEmail, senderName) {
+  return new Promise((fulfill, reject) => {
+    const data = {
+      from: {
+        email: senderEmail,
+        name: senderName
+      },
+      subject: 'Netlify Function - Sendgrid Email',
+      to: 'clive@theportman.co',
+      html: `Hey, you\'ve sent an email from Netlify Functions<br/>Message: ${message}`
+    }
+
+    client
+      .send(data)
+      .then(([response, body]) => {
+        fulfill(response)
+      })
+      .catch(error => reject(error))
+  })
+}
+
+exports.handler = function(event, context, callback) {
+
+  // const body = JSON.parse(event.body)
+  // const message = body.message
+
+  sendEmail(
+    client,
+    'This is a message coded into the function',
+    'clive@theportman.co',
+    'Clive Portman'
+  )
+  .then(response => callback(null, { statusCode: response.statusCode }))
+  .catch(err => callback(err, null))
+}
