d644470e04f3f9915d9acaa4ee8303b2fdc77079
cliveportman <clive@cliveportman.co.uk>
2020-11-16 12:21:37 +0000

    Update Homepage.svelte

--- src/Pages/Homepage.svelte
+++ src/Pages/Homepage.svelte
@@ -93,7 +93,7 @@
           <span class="line2" data-aos="fade-right" data-aos-delay="3000">Integrated</span>
       </div>
     </h1>
-    <video src="{videoUrl ? videoUrl : 'https://s3.eu-west-1.amazonaws.com/cogentia/images/cogentia-landscape.mp4?mtime=20201103110405&focal=none'}" bind:playbackRate="{speed}" bind:paused="{paused}" bind:duration="{duration}" bind:this={videoElement} allow="autoplay" muted  poster="/img/video-placeholder.jpg">
+    <video src="{videoUrl ? videoUrl : 'https://s3.eu-west-1.amazonaws.com/cogentia/images/cogentia-landscape.mp4?mtime=20201103110405&focal=none'}" bind:playbackRate="{speed}" bind:paused="{paused}" bind:duration="{duration}" bind:this={videoElement} allow="autoplay" muted  poster="/img/video-placeholder.jpg" playsinline>
       <track kind="captions">
     </video>
   </section>
